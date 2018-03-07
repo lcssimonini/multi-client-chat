2526f85f8a94edddac2d1851ad8269c7fede0269
lcssimonini <lcs.simonini@gmail.com>
2018-03-06 21:49:00 -0300

    iniciando

---
+++ src/main/java/br/com/simonini/multiclientchat/MultiClientChatApplication.java
@@ -0,0 +1,12 @@
+package br.com.simonini.multiclientchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiClientChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiClientChatApplication.class, args);
+	}
+}
